8c270b50333f7fccf17afdcfde54b2f4811c0d6e
SergheiCucereavii <serghei.cucereavii@r2u.it>
2024-03-07 16:22:01 +0100

    Add sql

---
+++ db/Dump20240307.sql
@@ -0,0 +1,186 @@
+-- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: exer
+-- ------------------------------------------------------
+-- Server version	8.0.36
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `_user`
+--
+
+DROP TABLE IF EXISTS `_user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `_user` (
+  `id` int NOT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `firstname` varchar(255) DEFAULT NULL,
+  `lastname` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `role` enum('USER','ADMIN') DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `_user`
+--
+
+LOCK TABLES `_user` WRITE;
+/*!40000 ALTER TABLE `_user` DISABLE KEYS */;
+INSERT INTO `_user` VALUES (1,'sergheicucereavi@gmail.com','Serghei','Cucereavii','$2a$10$lfWHBCJHr2Pz656dfXfUhuv9n2kyXRlGjXPZ3kKKv0uRPfLkhbIiS',NULL);
+/*!40000 ALTER TABLE `_user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `_user_seq`
+--
+
+DROP TABLE IF EXISTS `_user_seq`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `_user_seq` (
+  `next_val` bigint DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `_user_seq`
+--
+
+LOCK TABLES `_user_seq` WRITE;
+/*!40000 ALTER TABLE `_user_seq` DISABLE KEYS */;
+INSERT INTO `_user_seq` VALUES (51);
+/*!40000 ALTER TABLE `_user_seq` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `categories`
+--
+
+DROP TABLE IF EXISTS `categories`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `categories` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `categories`
+--
+
+LOCK TABLES `categories` WRITE;
+/*!40000 ALTER TABLE `categories` DISABLE KEYS */;
+/*!40000 ALTER TABLE `categories` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `commenti`
+--
+
+DROP TABLE IF EXISTS `commenti`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `commenti` (
+  `approved` tinyint(1) DEFAULT '0',
+  `user_id` int DEFAULT NULL,
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `parent_comment_id` bigint DEFAULT NULL,
+  `post_id` bigint NOT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK9cmqv4tprs228pq2s99jeefl9` (`parent_comment_id`),
+  KEY `FKspd3vtbmtkefd9mh7vncni4oa` (`post_id`),
+  KEY `FK58fyyrabsx5toijinqobfcl9g` (`user_id`),
+  CONSTRAINT `FK58fyyrabsx5toijinqobfcl9g` FOREIGN KEY (`user_id`) REFERENCES `_user` (`id`),
+  CONSTRAINT `FK9cmqv4tprs228pq2s99jeefl9` FOREIGN KEY (`parent_comment_id`) REFERENCES `commenti` (`id`),
+  CONSTRAINT `FKspd3vtbmtkefd9mh7vncni4oa` FOREIGN KEY (`post_id`) REFERENCES `posts` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `commenti`
+--
+
+LOCK TABLES `commenti` WRITE;
+/*!40000 ALTER TABLE `commenti` DISABLE KEYS */;
+/*!40000 ALTER TABLE `commenti` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `post_category`
+--
+
+DROP TABLE IF EXISTS `post_category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `post_category` (
+  `category_id` bigint NOT NULL,
+  `post_id` bigint NOT NULL,
+  KEY `FKmb0mart0q43x8cgtxlcjrav0h` (`category_id`),
+  KEY `FKlhcehr18f04qdenj715f0518i` (`post_id`),
+  CONSTRAINT `FKlhcehr18f04qdenj715f0518i` FOREIGN KEY (`post_id`) REFERENCES `posts` (`id`),
+  CONSTRAINT `FKmb0mart0q43x8cgtxlcjrav0h` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `post_category`
+--
+
+LOCK TABLES `post_category` WRITE;
+/*!40000 ALTER TABLE `post_category` DISABLE KEYS */;
+/*!40000 ALTER TABLE `post_category` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `posts`
+--
+
+DROP TABLE IF EXISTS `posts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `posts` (
+  `user_id` int DEFAULT NULL,
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `description` varchar(255) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKfeyahhljdty467fx2801oco30` (`user_id`),
+  CONSTRAINT `FKfeyahhljdty467fx2801oco30` FOREIGN KEY (`user_id`) REFERENCES `_user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `posts`
+--
+
+LOCK TABLES `posts` WRITE;
+/*!40000 ALTER TABLE `posts` DISABLE KEYS */;
+/*!40000 ALTER TABLE `posts` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-03-07 16:20:31
